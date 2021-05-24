9b38ca57ec56da15f0392802cf89921729329514
avagyansamvel07@gmail.com <avagyansamvel07@gmail.com>
2021-05-24 11:47:48 -0700

    Some changes

--- MedicalConsultation/MedicalConsultation/Models/Register.cs
+++ MedicalConsultation/MedicalConsultation/Models/Register.cs
@@ -1,9 +1,5 @@
 ﻿using MedicalConsultation.Areas.Identity.Data;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MedicalConsultation.Models
 {
--- MedicalConsultation/MedicalConsultation/Startup.cs
+++ MedicalConsultation/MedicalConsultation/Startup.cs
@@ -1,14 +1,8 @@
-using MedicalConsultation.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MedicalConsultation
 {
@@ -26,21 +20,11 @@ namespace MedicalConsultation
         {
             services.AddControllersWithViews();
             services.AddRazorPages();
-            //services.AddDbContext<MedConsContext>(options =>
-            //        options.UseSqlServer(Configuration.GetConnectionString("MedConsContextConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapRazorPages();
-            });
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -48,16 +32,22 @@ namespace MedicalConsultation
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
             }
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
